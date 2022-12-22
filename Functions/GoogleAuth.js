747dfcd677f390bdd19cfbf066ff06e697c652c4
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-22 20:43:35 +0530

    Update#1

--- Functions/GoogleAuth.js
+++ Functions/GoogleAuth.js
@@ -1,12 +1,19 @@
-import { google } from "googleapis";
-
 const clientSecret = "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";
 const clientId = "250276764654-8sf0mbais31hcomjktjj3uvj0b9n4phu.apps.googleusercontent.com";
 const redirectUrl = "http://localhost:8080/googlesignin/token";
-const scopes = ['https://www.googleapis.com/auth/youtube.readonly'];
+const scopes = ['https://www.googleapis.com/auth/youtube'];
+const yt_api = 'AIzaSyCE1j_qLjBNZoe5MRV2IGTvHDPTrXqOTzE';
 
+
+
+import { google} from "googleapis";
 
 export const oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+export const youtube = google.youtube({
+  version: 'v3',
+  auth: yt_api,
+});
 
 export const GetToken = async ()=>{
   try {
@@ -21,7 +28,7 @@ export const GetToken = async ()=>{
 } 
 
 export const SetToken = async (code) =>{
-  const {tokens} = await oauth2Client.getToken(code)
+  const {tokens} = await oauth2Client.getToken(code);
   oauth2Client.setCredentials(tokens);
   return;
 }
---
+++ Functions/Youtube_Data.js
@@ -0,0 +1,54 @@
+import {youtube} from './GoogleAuth.js';
+
+export const search_videos = async (query,value)=>{
+    let results = await youtube.search.list(
+        {
+            part:'snippet', 
+            q: query, 
+            maxResults: value,
+        });
+    return results.data.items;
+}
+
+export const popular_videos = async (value)=>{
+    let results = await youtube.videos.list(
+    {   part:'snippet',
+        maxResults : value,
+        chart : 'mostPopular',
+    });
+    
+    return {result : results.data.items, nextpagetoken : results.data.nextPageToken , prevpagetoken : results.data.prevPageToken};
+}
+
+export const popular_videos_by_pagetoken = async (pagetoken)=>{
+    let results = await youtube.videos.list({
+        part:'snippet',
+        maxResults : 25,
+        chart : 'mostPopular',
+        pageToken : pagetoken,
+    });
+    return {result : results.data.items, nextpagetoken : results.data.nextPageToken , prevpagetoken : results.data.prevPageToken};
+}
+
+
+export const user_liked_videos = async (value)=>{
+    let results = await youtube.videos.list(
+    {   part:'snippet',
+        maxResults : value,
+        myRating : 'liked',
+    });
+
+    return {result : results.data.items, nextpagetoken : results.data.nextPageToken , prevpagetoken : results.data.prevPageToken};
+}
+
+export const user_liked_videos_by_pagetoken = async (pagetoken)=>{
+    let results = await youtube.videos.list({
+        part:'snippet',
+        maxResults : 25,
+        myRating : 'liked',
+        pageToken : pagetoken,
+    });
+    return {result : results.data.items, nextpagetoken : results.data.nextPageToken , prevpagetoken : results.data.prevPageToken};
+}
+
+// yt url = https://www.youtube.com/watch?v=${videoid}
