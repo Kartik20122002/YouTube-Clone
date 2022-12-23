70ea7283ee1ed7388da61ea6fb5578eca44851b5
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-23 17:43:01 +0530

    db connectd

---
+++ DataBase/db.js
@@ -0,0 +1,42 @@
+import env from "dotenv";
+env.config();
+
+import mongoose from "mongoose";
+
+mongoose.set('strictQuery', false);
+//Set up default mongoose connection
+
+export const DB = "mongodb+srv://kartikhatwar98:9371865060k@youtube-lite.rcj46s0.mongodb.net/YouTube-Lite?retryWrites=true&w=majority";
+
+export const connectDB = ()=>{
+    mongoose.connect(DB,{
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }).then(()=>{
+        console.log('connected');
+    }).catch((err)=>{
+        console.log('Not connected :',err);
+    });
+}
+
+const userSchema = new mongoose.Schema({
+    id : {
+        type : String ,
+        default : null
+    },
+    email : {
+        type : String ,
+        required : [true , 'Email Required'],
+        unique : [true,'Email Already Registered']
+    },
+    firstName : String,
+    lastName : String,
+    profilePhoto : String,
+    password : {
+        type : String,
+    },
+    source: { type: String, required: [true, "source not specified"] },
+    lastVisited: { type: Date, default: new Date() }
+});
+
+export const userModel = mongoose.model("user", userSchema, "user");
