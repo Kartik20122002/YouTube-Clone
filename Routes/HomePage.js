f62d725373d0d8644656118a118fe7d34c95b58b
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-23 15:16:39 +0530

    routes

--- Routes/HomePage.js
+++ Routes/HomePage.js
@@ -4,6 +4,14 @@ export const Home = express.Router();
 Home.use(express.json());
 
 Home
-.get('/',(req,res)=>{
-    res.render('HomePage.ejs');
-})
+.get('/',async (req,res)=>{
+   
+    let {result , nextpagetoken , prevpagetoken}= await popular_videos(25);
+    ejs.renderFile('./views/HomePage.ejs',{items : result , nextpagetoken : nextpagetoken , prevpagetoken : prevpagetoken},{},(err,temp)=>{
+        if (err) {
+            throw err;
+        } else {
+            res.end(temp);
+        }
+    });
+});
