457112c45971da82d8bb6587421943200005e025
Kartik Hatwar <kartikhatwar98@gmail.com>
2022-12-26 14:25:21 +0530

    try#1

--- Functions/GoogleAuth.js
+++ Functions/GoogleAuth.js
@@ -6,4 +6,4 @@ export const clientId = process.env.CLIENTID
 export const redirectUrl = process.env.REDIRECTURL
 export const scopes = ['https://www.googleapis.com/auth/youtube','profile'];
 export const yt_api = process.env.YT_API
-
+export const secret = process.env.SECRET
--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
+import { secret } from './Functions/GoogleAuth.js';
 import { Auth, isLoggedIn } from './Routes/Auth.js';
 import { Home } from './Routes/HomePage.js';
 import { SearchResults } from './Routes/SearchResults.js';
 
 const app = express();
-const port = process.env.PORT || 8080;
-
-app.use(session({secret : process.env.SECRET}));
+app.use(session({secret : secret}));
 app.use(passport.initialize());
 app.use(passport.session());
+
+const port = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 app.use(express.static('./views'));
 app.set('views','./views');
 app.use(express.urlencoded({ extended: true }))
 
-
-app.use('/',isLoggedIn,Home);
+app.get('/',isLoggedIn,Home);
 app.use('/googleauth',Auth);
 app.use('/searchresults',isLoggedIn,SearchResults);
 
 app.listen(port,()=>{
-    // console.log(`https://youtube-lite.onrender.com`);
+    console.log(`http://localhost:${port}/`);
 });
