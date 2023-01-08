44ef42ec9e4f6d1e226ce0e025b824f12091890d
Kartik Hatwar <kartikhatwar98@gmail.com>
2023-01-08 16:39:40 +0530

    offline page added

--- views/service_worker.js
+++ views/service_worker.js
@@ -2,6 +2,9 @@ const static_cache = 'static_cache';
 const dyanamic_cache = 'dyanamic_cache';
 
 const assets = [
+    '/',
+    'OfflinePage.html',
+    'css/OfflinePage.css',
     'LoginPage.ejs',
     'HomePage.ejs',
     'css/LoginPage.css',
@@ -45,6 +48,8 @@ self.addEventListener('install', function(event) {
       event.respondWith(
            caches.match(event.request).then(cacheRes =>{
             return cacheRes || fetch(event.request);
+           }).catch(()=> caches.match('OfflinePage.html')).then(cacheRes=>{
+            return cacheRes;
            })
     )
   });
@@ -61,6 +66,6 @@ self.addEventListener('install', function(event) {
             })
             .then(() => self.clients.claim())
         )
-  })
+  });
 
 
