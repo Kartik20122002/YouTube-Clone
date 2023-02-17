9139965fa6675225b79e560a9a92c6e37e780ab6
Kartik Hatwar <kartikhatwar98@gmail.com>
2023-02-17 13:10:30 +0530

    Service Worker Removed

--- views/service_worker.js
+++ views/service_worker.js
@@ -1,41 +1,18 @@
 const static_cache = 'static_cache';
 const dyanamic_cache = 'dyanamic_cache';
 
-const assets = [
-    
-]
-
 self.addEventListener('install', (event)=> {
       event.waitUntil(
       (async ()=>{
-        cache = await caches.open(static_cache);
-        await cache.addAll(assets);
         self.skipWaiting();
       })
       )
     
   })
   
-  self.addEventListener('fetch', function(event) {
-      event.respondWith(
-           caches.match(event.request).then(cacheRes =>{
-            return cacheRes || fetch(event.request).then((res)=>{
-              return res;
-            });
-           }));
-  });
-  
   self.addEventListener('activate', function(event) {
       event.waitUntil(
-          caches.keys()
-            .then((keyList) => {
-              return Promise.all(keyList.map((key) => {
-                if (key !== static_cache) {
-                  return caches.delete(key)
-                }
-              }))
-            })
-            .then(() => self.clients.claim())
+        self.clients.claim()
         )
   });
 
